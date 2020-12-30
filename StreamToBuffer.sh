add6b641912bdaf7a0cf6bd145806ee752ba653e
goshrow <abhroindia@gmail.com>
2020-12-31 00:25:23 +0530

    Redirection onto Buffer implemented

--- StreamToBuffer.sh
+++ StreamToBuffer.sh
@@ -4,14 +4,15 @@ getDevices() {
     # deviceIDArray=("${(@f)$(xinput list --id-only)}") 
     # Use the previous line for a simpler implementation in zsh
 
-    xinput list --id-only >> DevicesList
+    xinput list --id-only >> DevicesIDListFile
     # bash 4.0+ could go for mapfile or readarray. 
     # For backward compatibility, read the file onto an array explicitly.
     local deviceIDArray=()
     while IFS= read -r line; do
         deviceIDArray+=("$line")
-    done < DevicesList
-    rm DevicesList
+        
+    done < DevicesIDListFile
+    rm DevicesIDListFile
     echo ${deviceIDArray[*]}
 }
 
@@ -19,17 +20,26 @@ getRedirectCommand() {
     # returns an to-be evaluated command which redirects xinput from Devices
     local deviceIDArray=$(getDevices)
     local redirectCommand=""
+    local BufferFile="$1"
     for deviceID in ${deviceIDArray[@]}; do
-        redirectCommand+="xinput test $deviceID >> myfile && "
+        redirectCommand+="xinput test $deviceID >> $BufferFile & "
     done
     if [ -z "$redirectCommand" ]; then 
         printf "No Devices found to be added to the Redirection Buffer"
     else
         echo $redirectCommand >> redirectCommandFile
-        redirectCommand=$(rev redirectCommandFile | cut -c 4- | rev)
+        redirectCommand=$(rev redirectCommandFile | cut -c 3- | rev)
         rm redirectCommandFile
     fi
     echo $redirectCommand
 }
 
-getRedirectCommand
+initDeviceInputRedirect() {
+    killall -q xinput
+    # starts the process unaffected by previous run
+    local redirectCommand=$(getRedirectCommand Buffer1)
+    eval $redirectCommand
+}
+
+
+initDeviceInputRedirect
